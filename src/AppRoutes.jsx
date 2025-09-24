fcfb6cb993b64749252fe5fc35c0c49c7c8c02ab
theshubhamboss <shubhamboss59@gmail.com>
2025-09-24 23:41:15 +0530

    added basic pages daisyui theming

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./pages/landing/LandingPage";
 import LoginPage from "./pages/login/LoginPage";
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function AppRouter() {
   return (
     <>
       <BrowserRouter>
+      <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
